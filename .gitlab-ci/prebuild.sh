735f54c9afe27644c0dd628177d3575afdf934c2
Cédric DELAMARRE <cedric.delamarre@3ds.com>
2024-09-25 13:08:37 +0000

    add nlohmann json in prerequirements
    See https://gitlab.com/scilab/scilab/-/merge_requests/1060

--- .gitlab-ci/prebuild.sh
+++ .gitlab-ci/prebuild.sh
@@ -78,6 +78,7 @@ XZ_VERSION=5.4.4
 JOGL_VERSION=2.5.0
 OPENXLSX_VERSION=0.3.2
 LIBARCHIVE_VERSION=3.7.1
+NLOHMANN_JSON_VERSION=3.11.3
 
 # CppServer and its deps
 CPPSERVER_VERSION=1.0.4.1
@@ -117,6 +118,7 @@ make_versions() {
     echo "JOGL_VERSION          = $JOGL_VERSION"
     echo "OPENXLSX_VERSION      = $OPENXLSX_VERSION"
     echo "LIBARCHIVE_VERSION    = $LIBARCHIVE_VERSION"
+    echo "NLOHMANN_JSON_VERSION = $NLOHMANN_JSON_VERSION"
     echo "CPPSERVER_VERSION     = $CPPSERVER_VERSION"
     echo "ASIO_VERSION          = $ASIO_VERSION"
     echo "CPPCOMMON_VERSION     = $CPPCOMMON_VERSION"
@@ -176,6 +178,8 @@ download_dependencies() {
 
     [ ! -f libarchive-$LIBARCHIVE_VERSION.tar.xz ] && curl -LO https://oos.eu-west-2.outscale.com/scilab-releases-dev/prerequirements-sources/libarchive-$LIBARCHIVE_VERSION.tar.xz
 
+    [ ! -f nlohmann_json-$NLOHMANN_JSON_VERSION.hpp ] && curl -LO https://oos.eu-west-2.outscale.com/scilab-releases-dev/prerequirements-sources/nlohmann_json-$NLOHMANN_JSON_VERSION.hpp
+
     # CppServer and its deps
     [ ! -f cppserver-$CPPSERVER_VERSION.zip ] && curl -LO https://oos.eu-west-2.outscale.com/scilab-releases-dev/prerequirements-sources/cppserver-$CPPSERVER_VERSION.zip
     [ ! -f asio-$ASIO_VERSION.zip ] && curl -LO https://oos.eu-west-2.outscale.com/scilab-releases-dev/prerequirements-sources/asio-$ASIO_VERSION.zip
@@ -209,6 +213,7 @@ make_all() {
     build_ncurses
     build_openxlsx
     build_libarchive
+    build_nlohmann_json
     build_cppserver
 }
 
@@ -857,6 +862,10 @@ build_libarchive() {
 
     cp -a "$INSTALL_DIR"/lib/*.so* "$INSTALLUSRDIR/lib/"
     cp -a "$INSTALL_DIR"/include/* "$INSTALLUSRDIR/include/"
+}
+
+build_nlohmann_json() {
+    cp -a "$DOWNLOADDIR"/nlohmann_json-$NLOHMANN_JSON_VERSION.hpp "$INSTALLUSRDIR/include/nlohmann_json.hpp"
 }
 
 build_suitesparse() {
